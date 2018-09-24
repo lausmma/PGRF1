afd02b78b64b119fe418e62ae58bc97e21cfd616
lausmma <marek.lausman@seznam.cz>
2018-09-24 12:14:01 +0200

    BufferedImg - doesnt work

---
+++ src/ui/PgrfFrame.java
@@ -0,0 +1,36 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class PgrfFrame extends JFrame {
+
+    private BufferedImage img;
+    static int width = 800;
+    static int height = 600;
+
+    public static void main(String... args){
+
+        PgrfFrame pgrfFrame = new PgrfFrame();
+        pgrfFrame.img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        pgrfFrame.init(width, height);
+    }
+
+    private void init(int width, int height){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(width, height);
+        setTitle("Počítačová grafika");
+
+        draw();
+    }
+
+    private void draw(){
+        img.getGraphics().fillRect(0, 0, img.getWidth(), img.getHeight());
+        for (int i = 0; i < 100; i++) {
+            img.setRGB(200 + i, 200, Color.RED.getRGB());
+        }
+        getGraphics().drawImage(img, 0, 0,null);
+    }
+}
